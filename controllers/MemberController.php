b5590d4322e205053e81beae48638916ede61277
hiccphp <hansongchao2014@gmail.com>
2018-08-06 18:58:58 +0800

    member login function implements

--- controllers/MemberController.php
+++ controllers/MemberController.php
@@ -7,6 +7,30 @@ class MemberController extends BaseController
 
     public function actionAuth()
     {
-        return $this->render('auth.php');
+        $this->layout = 'layout2';
+        if (Yii::$app->request->isGet) {
+            $url = Yii::$app->request->referrer;
+            if (empty($url)) {
+                $url = "/";
+            }
+            Yii::$app->session->setFlash('referrer', $url);
+        }
+        $model = new User;
+        if (Yii::$app->request->isPost) {
+            $post = Yii::$app->request->post();
+            if ($model->login($post)) {
+                return $this->redirect($url);
+            }
+        }
+        return $this->render('auth', ['model' => $model]);
+    }
+
+    public function actionLogout()
+    {
+        Yii::$app->session->remove('loginname');
+        Yii::$app->session->remove('isLogin');
+        if (!isset(Yii::$app->session['isLogin'])) {
+            return $this->goback(Yii::$app->request->referrer);
+        }
     }
 }
